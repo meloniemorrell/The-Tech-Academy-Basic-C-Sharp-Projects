150128595f9e92f768369a3cb91370210e7ce292
meloniemorrell <meloniemorrell73@gmail.com>
2020-09-28 01:21:21 -0700

    added assignments

---
+++ assignment_151_4/assignment_151_6.sln
@@ -0,0 +1,18 @@
+﻿using System;
+public class Exercise6
+{
+    public static void Main()
+    {
+
+
+        Console.WriteLine("Input a number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        bool trueOrFalse = a > 50 ;
+        Console.WriteLine(trueOrFalse.ToString());
+        Console.ReadLine();
+
+
+
+
+    }
+}
---
+++ assignment_152/assignment_152/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+class Program
+{
+
+    static void Main()
+    {
+        Console.WriteLine("Anonymous Income Comparison Program");
+        Console.ReadLine();
+        Console.WriteLine("Person 1");
+        Console.ReadLine();
+        Console.WriteLine("Enter hourly rate: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter hours worked per week: ");
+        int b= Convert.ToInt32(Console.ReadLine());
+        int s1 = (a * b *52);
+        string msg = "Annual salary for Person 1 is " + s1.ToString();
+        Console.WriteLine(msg);
+        Console.ReadLine();
+
+        Console.WriteLine("Person 2");
+        Console.ReadLine();
+        Console.WriteLine("Enter hourly rate: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter hours worked per week: ");
+        int d = Convert.ToInt32(Console.ReadLine());
+        int s2 = (c * d * 52);
+        string msg1 = "Annual salary for Person 2 is " + s2.ToString();
+        Console.WriteLine(msg1);
+        Console.ReadLine();
+
+        Console.WriteLine("Does person 1 make more money than person 2? ");
+        bool trueOrFalse = s1 > s2;
+        Console.WriteLine(trueOrFalse.ToString());
+        Console.ReadLine();
+
+
+
+    }
+}
