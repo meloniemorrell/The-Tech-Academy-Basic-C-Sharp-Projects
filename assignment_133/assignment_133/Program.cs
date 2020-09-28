96e072e0cf2aa6693fd9d7d47af4ff8a2b80136c
meloniemorrell <meloniemorrell73@gmail.com>
2020-09-27 18:23:59 -0700

    made changes to 133

---
+++ assignment_133/assignment_133/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("The Tech Academy");
+        string TechAcad = Console.ReadLine();
+        Console.ReadLine();
+
+        Console.WriteLine("Student Daily Report");
+        string DailyRep = Console.ReadLine();
+        Console.ReadLine();
+
+        Console.WriteLine("What is your name?");
+        string yourName = Console.ReadLine();
+        Console.WriteLine("Your name is " + yourName);
+        Console.ReadLine();
+
+        Console.WriteLine("What course are you on?");
+        string yourCourse = Console.ReadLine();
+        Console.WriteLine("Your course is " + yourCourse);
+        Console.ReadLine();
+
+
+        Console.WriteLine("What page number?");
+        string yourPage = Console.ReadLine();
+        Console.WriteLine("Your page number is " + yourPage);
+        Console.ReadLine();
+
+        Console.WriteLine("Do you need help with anything? Please answer “true” or “false.”? ");
+        string helpNohelp = Console.ReadLine();
+        Console.ReadLine();
+
+        Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics");
+        string yourXperience = Console.ReadLine();
+        Console.ReadLine();
+
+
+        Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+        string yourFeedback = Console.ReadLine();
+        Console.ReadLine();
+
+
+        Console.WriteLine("How many hours did you study today?");
+        string hoursStudied = Console.ReadLine();
+        Console.ReadLine();
+
+
+    }
+}
+
