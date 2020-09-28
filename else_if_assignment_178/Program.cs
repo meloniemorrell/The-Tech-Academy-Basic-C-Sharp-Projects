379486a281e414212f4fc882310c6ecca1b8c6ce
meloniemorrell <meloniemorrell73@gmail.com>
2020-09-28 13:41:30 -0700

    added assignments

--- else_if_assignment_178/Program.cs
+++ else_if_assignment_178/Program.cs
@@ -22,32 +22,45 @@ namespace else_if_assignment_178
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
             }
-            break;
 
-            Console.WriteLine("Please enter package width: ");
-            int wi = Convert.ToInt32(Console.ReadLine());
+            else if (wt < 50)
+            {
+                Console.WriteLine("Please enter package width: ");
+                int wi = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please enter package height: ");
-            int h = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter package height: ");
+                int h = Convert.ToInt32(Console.ReadLine());
 
 
-            Console.WriteLine("Please enter package length: ");
-            int l = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter package length: ");
+                int l = Convert.ToInt32(Console.ReadLine());
 
-        
-
-            if (wi * h * l > 50) 
+                int totalWt = ((wi * h * l) * wt / 100);
+                string msg = "The quote for your shipping charges is $ " + totalWt.ToString();
+                Console.ReadLine();
+            }
+          
+            else if (wi * h * l > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express.");
 
-                break;
+            }
+
+
+
+
+
+
+
+            {
+
+               
                                 
             }
                     
            
 
-            int totalWt= ((l * wi * h) * wt / 100);
-            string msg = "The quote for your shipping charges is $ " + totalWt.ToString();
+            
 
 
 
