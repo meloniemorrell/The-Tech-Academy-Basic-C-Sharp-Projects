69c5b2fc5905d4f5962680ec45a7bebea983c27c
meloniemorrell <meloniemorrell73@gmail.com>
2020-09-29 12:34:31 -0700

    added assignments

--- array_assignment_191_2/array_assignment_191_2/Program.cs
+++ array_assignment_191_2/array_assignment_191_2/Program.cs
@@ -15,7 +15,7 @@ namespace arrays_lists_assignments
             int[] numArray = new int[14];
 
             numArray[0] = 1;
-            numArray[1] = 2;
+            numArray[1] = 23;
             numArray[2] = 3;
             numArray[3] = 4;
             numArray[4] = 5;
@@ -29,42 +29,14 @@ namespace arrays_lists_assignments
             numArray[12] = 13;
             numArray[13] = 14;
            
-            Console.WriteLine("Select the date I was born from the list: ");
+            Console.WriteLine("Select an index of the array: ");
+
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numArray[input1]);
             Console.ReadLine();
 
-            Console.WriteLine("1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14: ");
-            Console.ReadLine();
-
-     
-            Console.WriteLine(numArray[0] + " ,Sorry try again!");
-            Console.ReadLine();
-            Console.WriteLine(numArray[1] + ", Sorry try again!");
-            Console.ReadLine();
-            Console.WriteLine(numArray[2] + ", Sorry try again!");
-            Console.ReadLine();
-            Console.WriteLine(numArray[3] + ", Sorry try again!");
-            Console.ReadLine();
-            Console.WriteLine(numArray[4] + ", Sorry try again!");
-            Console.ReadLine();
-            Console.WriteLine(numArray[5] + ", Sorry try again!");
-            Console.ReadLine();
-            Console.WriteLine(numArray[6] + ", Sorry try again!");
-            Console.ReadLine();
-            Console.WriteLine(numArray[7] + ", Sorry try again!");
-            Console.ReadLine();
-            Console.WriteLine(numArray[8] + ", Sorry try again!");
-            Console.ReadLine();
-            Console.WriteLine(numArray[9] + ", Sorry try again!");
-            Console.ReadLine();
-            Console.WriteLine(numArray[10] + ", Sorry try again!");
-            Console.ReadLine();
-            Console.WriteLine(numArray[11] + " ,yes that is correct!");
-            Console.ReadLine();
-            Console.WriteLine(numArray[12 ] + ", Sorry try again!");
-            Console.ReadLine();
-            Console.WriteLine(numArray[13] + ", Sorry try again!");
-            Console.ReadLine();
-
+            
+           
 
 
         }
