f44bf3c49478950e7ede5a2d67b0fc63fb3385fd
meloniemorrell <meloniemorrell73@gmail.com>
2020-09-29 14:25:46 -0700

    assignment resubmission

--- boolean_assignment_161/boolean_assignment_161/Program.cs
+++ boolean_assignment_161/boolean_assignment_161/Program.cs
@@ -4,12 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace boolean_assignment_161
+namespace assignment_boolean_161
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your age?");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI, True or False? ");
+            bool trueOrFalse = Convert.ToBoolean(Console.ReadLine());
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have? ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            bool a = (num1 > 15 && (trueOrFalse == false) && num3 < 3);
+
+
+            if (a == true)
+            {
+                Console.WriteLine("Approved");
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("Not approved");
+                Console.ReadLine();
+            }
+
+
+
+
+
+
         }
+
+
+
+
     }
 }
