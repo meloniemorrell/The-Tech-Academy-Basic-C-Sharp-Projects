837c975133a62cddf06a8520c0586205f1ff0635
meloniemorrell <meloniemorrell73@gmail.com>
2020-10-02 17:01:49 -0700

    added assignments

---
+++ class_methods_assignment_216/class_methods_assignment_216/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    public class Calculator
+    {
+
+            public int Add(int num1)
+            {
+                int m_result = num1 + 2;
+                return m_result;
+            }
+            public int Multiply(int num1)
+            {
+               int m_result = num1 * 2;
+                return m_result;
+            }
+            public int Subtract(int num1)
+            {
+                int m_result = num1 - 2;
+                return m_result;
+            }
+       
+
+
+        
+    }
+}         
+
+
+             
+            
+
+
+
---
+++ class_methods_assignment_216/class_methods_assignment_216/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number to perform math operations on: ");
+            int userinput = Convert.ToInt32(Console.ReadLine());
+
+            Calculator calc = new Calculator();
+
+            int result = calc.Add(userinput);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            int result2 = calc.Multiply(userinput);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+            int result3 = calc.Subtract(userinput);
+            Console.WriteLine(result3);
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+}
+
+    
