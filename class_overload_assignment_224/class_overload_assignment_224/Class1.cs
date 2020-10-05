ce0d6e46a75469c0b6e58e3927e68db359b71316
meloniemorrell <meloniemorrell73@gmail.com>
2020-10-04 23:12:41 -0700

    added assignment

---
+++ class_overload_assignment_224/class_overload_assignment_224/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace class_overload_assignment_224
+{
+    public static class Calculator
+    {
+        public static void DivideBy2(int n1)
+        {
+            int result = n1 / 2;
+            Console.WriteLine(result);
+        }
+        public static void DivideBy2(int n1, out int n2)
+        {
+            n2 = n1 / 2;
+            Console.WriteLine(n2);
+        }
+    }
+}
---
+++ class_overload_assignment_224/class_overload_assignment_224/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace class_overload_assignment_224
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var calculator = new Calculator();
+            Console.WriteLine("Enter an integer");
+            var userInput = Console.ReadLine();
+
+            var inputAsInt = Convert.ToInt32(userInput);
+
+            Calculator.DivideBy2(inputAsInt);
+
+            int outputValue;
+            Calculator.DivideBy2(inputAsInt, out outputValue);
+            var userInput1 = Console.ReadLine();
+
+
+        }
+
+    }
+}
