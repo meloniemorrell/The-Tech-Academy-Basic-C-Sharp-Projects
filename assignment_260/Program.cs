24d62c0128651d6e6e349bdfdd09dc4f8cdfc47f
meloniemorrell <meloniemorrell73@gmail.com>
2020-10-08 06:46:28 -0700

    added assignment

---
+++ assignment_260/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_260
+{
+    struct Number
+    {
+        public decimal Amount;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number n;
+            n.Amount = 1.5m;
+            Console.WriteLine(n.Amount.ToString("n2"));
+            Console.ReadLine();
+        }
+    }
+}
