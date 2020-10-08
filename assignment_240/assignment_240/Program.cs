7a6015361b1d2e652d90063f4b2ddb8066e96ac4
meloniemorrell <meloniemorrell73@gmail.com>
2020-10-08 06:32:02 -0700

    added assignments

---
+++ assignment_240/assignment_240/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_240
+{
+    interface IQuittable
+    {
+        void Quit();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();
+            e.Quit();
+            Console.ReadLine();
+        }
+    }
+
+    public class Employee : IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("I am using polymorphism to create an object of type IQuittable");
+        }
+    }
+}
+
