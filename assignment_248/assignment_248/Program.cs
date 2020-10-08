c9e1c65b63293e514f089471f8d67f54070b61aa
meloniemorrell <meloniemorrell73@gmail.com>
2020-10-08 06:41:30 -0700

    added assignment

---
+++ assignment_248/assignment_248/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_248
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> e = new Employee<string>();
+            e.things = new List<string> { "hello", "goodbye", "here" };
+            foreach (string s in e.things)
+            {
+                Console.WriteLine(s);
+            }
+
+            Employee<int> e2 = new Employee<int>();
+            e2.things = new List<int> { 1, 2, 3, 4 };
+            foreach (int a in e2.things)
+            {
+                Console.WriteLine(a.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Employee<T>
+    {
+        public List<T> things;
+    }
+
+}
+ 
