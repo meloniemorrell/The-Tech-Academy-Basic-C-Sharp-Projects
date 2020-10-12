6fb49b530a60a82b01491a56f494bd27d04d3968
meloniemorrell <meloniemorrell73@gmail.com>
2020-10-12 16:45:33 -0700

    added assignment

---
+++ MVCTutorial/MVCTutorial/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Random rnd = new Random(10);
+            int num = rnd.Next();
+
+            if (num > 20000)
+            {
+                return View("About");
+            }
+
+            return View();
+               
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TechAcadStudentsMVC/TechAcadStudentsMVC/Models/Instructor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechAcadStudentsMvc.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+        public int Id { get; internal set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
