db9f06651f512f1b22ecc17c389d3a3aacd0cb05
meloniemorrell <meloniemorrell73@gmail.com>
2020-10-15 19:59:43 -0700

    updates

--- NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
@@ -15,9 +15,9 @@ namespace NewsletterAppMVC.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
+        public ActionResult SignUp(string firstName, string lastName, string email)
         {
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(email))
             {
                 return View("~/Views/Shared/Error.cshtml");
             }
@@ -28,19 +28,19 @@ namespace NewsletterAppMVC.Controllers
                 string connectionString = @"Data Source=DESKTOP-MRMI8HG;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
                 
 
-                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
-                                        (@FirstName, @LastName, @EmailAddress)";
+                string queryString = @"INSERT INTO SignUps (FirstName, LastName, Email) VALUES
+                                        (@FirstName, @LastName, @Email)";
 
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     SqlCommand command = new SqlCommand(queryString, connection);
                     command.Parameters.Add("@FirstName", SqlDbType.VarChar);
                     command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+                    command.Parameters.Add("@Email", SqlDbType.VarChar);
 
                     command.Parameters["@FirstName"].Value = firstName;
                     command.Parameters["@LastName"].Value = lastName;
-                    command.Parameters["@EmailAddress"].Value = emailAddress;
+                    command.Parameters["@Email"].Value = email;
 
                     connection.Open();
                     command.ExecuteNonQuery();
