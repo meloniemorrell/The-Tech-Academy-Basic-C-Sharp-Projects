6063af48e154007495c9089aa72f57c53ddee959
meloniemorrell <meloniemorrell73@gmail.com>
2020-10-22 19:02:07 -0700

    updates

---
+++ assignment_254/assignment_254/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM254
+{
+    class Program
+    {
+        enum WeekDays
+        {
+            monday,
+            tuesday,
+            wednesday,
+            thursday,
+            friday,
+            saturday,
+            sunday
+        }
+
+        static void Main(string[] args)
+        {
+            bool inValidDay = true;
+            do
+            {
+                Console.Write("Enter the current day of the week? ");
+                string day = Console.ReadLine();
+                //conver to lower case
+                day = day.ToLower();
+
+                try
+                {
+                    WeekDays dow = (WeekDays)Enum.Parse(typeof(WeekDays), day);
+                    inValidDay = false;
+                }
+                catch (Exception )
+                {
+                    Console.WriteLine("Please enter an actual day of the week");
+                }
+            } while (inValidDay);
+        }
+    }
+}
