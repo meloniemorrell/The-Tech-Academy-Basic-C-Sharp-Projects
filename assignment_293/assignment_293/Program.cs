4afc4a9cdf8d356fcd5a5391f7045aeb66a7948f
meloniemorrell <meloniemorrell73@gmail.com>
2020-10-22 19:08:14 -0700

    added assignmnts

---
+++ assignment_293/assignment_293/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM293
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            Console.WriteLine(today.ToString("g"));
+            Console.Write("Please enter a number: ");
+            string input = Console.ReadLine();
+
+            //convert to integer
+            Int32.TryParse(input, out int number);
+            DateTime newDate = today.AddHours(number);
+            Console.WriteLine("The new date requested is: " + newDate.ToString("g"));
+            Console.ReadLine();
+        }
+    }
+}
