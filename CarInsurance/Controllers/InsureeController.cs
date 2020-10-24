f2beb27c31cfcf877d949fc5ba6832dd31506c2e
meloniemorrell <meloniemorrell73@gmail.com>
2020-10-24 08:47:13 -0700

    added and made updates

---
+++ CarInsurance/Controllers/InsureeController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class InsureeController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                //calculate quote
+                var quote = CalcQuote(insuree);
+                insuree.Quote = quote > 0 ? quote : insuree.Quote;
+                db.Insurees.Add(insuree);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insuree);
+        }
+
+        private decimal CalcQuote(Insuree insuree)
+        {
+            decimal baseRate = 50m;
+            var today = DateTime.Today;
+            // Calculate the age.
+            var age = today.Year - insuree.DateOfBirth.Year;
+            if (insuree.DateOfBirth > today.AddYears(-age)) age--;
+
+            var ageAddition = 0m;
+            if (age < 18)
+            {
+                ageAddition = 100m;
+            } else if (age > 18 && age < 26) {
+                ageAddition = 50m;
+            } else if (age > 25)
+            {
+                ageAddition = 25m;
+            }
+
+            var carAddition = 0m;
+            if (insuree.CarYear < 2000)
+            {
+                carAddition = 25m;
+            } else if (insuree.CarYear > 2015)
+            {
+                carAddition = 25m;
+            }
+
+            var makeAddition = 0m;
+            if (insuree.CarMake.Trim().ToLower() == "Porsche")
+            {
+                makeAddition = 25m;
+
+                if (insuree.CarModel.Trim().ToLower() == "911 Carrera")
+                {
+                    makeAddition += 25m;
+                }
+            }
+
+            var ticketAddition = 0m;
+            if (insuree.SpeedingTickets > 0)
+            {
+                ticketAddition = insuree.SpeedingTickets * 10;
+            }
+
+            var subTotal = baseRate + ageAddition + carAddition + makeAddition + ticketAddition;
+
+            if (insuree.DUI)
+            {
+                subTotal += (subTotal * .25m);
+            }
+
+            if (insuree.CoverageType)
+            {
+                subTotal += (subTotal * .50m);
+            }
+
+            return subTotal;
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(insuree).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insuree insuree = db.Insurees.Find(id);
+            db.Insurees.Remove(insuree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MM444/MM444/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace MM444
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                // Create and save a new Blog
+                Console.WriteLine("Please enter new student information: ");
+                Student s = new Student();
+
+                bool invalidInput = true;
+                invalidInput = true;
+
+                do
+                {
+                    Console.Write("Please enter the student's first name: ");
+                    string studentFirst = Console.ReadLine();
+                    if (String.IsNullOrWhiteSpace(studentFirst))
+                    {
+                        Console.WriteLine("Invalid first name, please try again.");
+                        continue;
+                    }
+                    s.FirstName = studentFirst;
+                    invalidInput = false;
+                } while (invalidInput);
+
+                invalidInput = true;
+                do
+                {
+                    Console.Write("Please enter the student's last name: ");
+                    string studentLast = Console.ReadLine();
+                    if (String.IsNullOrWhiteSpace(studentLast))
+                    {
+                        Console.WriteLine("Invalid last name, please try again.");
+                        continue;
+                    }
+                    s.LastName = studentLast;
+                    invalidInput = false;
+                } while (invalidInput);
+
+                invalidInput = true;
+                do
+                {
+                    Console.Write("Please enter the student's address: ");
+                    string studentAddress = Console.ReadLine();
+                    if (String.IsNullOrWhiteSpace(studentAddress))
+                    {
+                        Console.WriteLine("Invalid address, please try again.");
+                        continue;
+                    }
+                    s.Address = studentAddress;
+                    invalidInput = false;
+                } while (invalidInput);
+
+                invalidInput = true;
+                do
+                {
+                    Console.Write("Please enter the student's phone number: ");
+                    string studentPhone = Console.ReadLine();
+                    if (String.IsNullOrWhiteSpace(studentPhone))
+                    {
+                        Console.WriteLine("Invalid phone number, please try again.");
+                        continue;
+                    }
+                    s.Phone = studentPhone;
+                    invalidInput = false;
+                } while (invalidInput);
+
+                invalidInput = true;
+                do
+                {
+                    Console.Write("Please enter the student's GPA: ");
+                    string studentGPA = Console.ReadLine();
+                    Decimal.TryParse(studentGPA, out decimal decStudentGPA);
+                    if (decStudentGPA < 0m || decStudentGPA > 4.0m)
+                    {
+                        Console.WriteLine("Invalid GPA (must be between 0.0 and 4.0), please try again.");
+                        continue;
+                    }
+                    s.GPA = decStudentGPA;
+                    invalidInput = false;
+                } while (invalidInput);
+
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Please wait while we save the student to the database....");
+
+                db.Students.Add(s);
+                db.SaveChanges();
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Loading all currents students");
+                Console.WriteLine();
+                Console.WriteLine();
+
+                // Display all Blogs from the database
+                var query = from b in db.Students
+                            orderby b.Id
+                            select b;
+
+                Console.WriteLine("All students in the database:");
+                Console.Write(("Student ID").PadRight(30));
+                Console.Write(("Firstname").PadRight(30));
+                Console.Write(("Lastname").PadRight(30));
+                Console.Write(("GPA").PadRight(30));
+                foreach (var item in query)
+                {
+                    Console.Write(item.Id.ToString().PadRight(30));
+                    Console.Write(item.FirstName.PadRight(30));
+                    Console.Write(item.LastName.PadRight(30));
+                    Console.Write(item.GPA.ToString("n2").PadRight(30));
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public decimal GPA { get; set; }
+    }
+
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+
+}
